a6fa973ac1c49538432f8e1a0aad3ca9fab95edc
James Openshaw <jamesopenshaw4@gmail.com>
2021-09-14 20:19:07 +0200

    Update TarotReading.py

--- TarotReading.py
+++ TarotReading.py
@@ -3,6 +3,25 @@ import TarotDecks as td
 import time
 import webbrowser
 
+# Choosing a deck
+confirm = "No"
+while confirm != "Yes":
+
+    deck_choice = input("Which deck would you like to use? ")
+
+    if deck_choice == decks[0]:
+        print("You have chosen the Golden Dawn deck.")
+        confirm = input("Are you sure that this is the deck you want to use? ")
+    elif deck_choice == decks[1]:
+        print("You have chosen the Rider-Waite-Smith deck.")
+        confirm = input("Are you sure that this is the deck you want to use? ")
+    elif deck_choice == decks[2]:
+        print("You have chosen the Book of Thoth deck.")
+        confirm = input("Are you sure that this is the deck you want to use? ")
+    else:
+        print("Please input a valid deck name.")
+        confirm = "No"
+
 if td.deck_choice == "Golden Dawn":
     print("Please wait while your three cards are generated...")
     time.sleep(3)
